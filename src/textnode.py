9aa7f3c4d583bb9bfb9a385f3005edab08f55b4b
Nick Czarnecki <nicholas.czarnecki@gmail.com>
2025-01-11 19:07:30 -0500

    Initial setup

---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
