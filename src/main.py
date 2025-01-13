6511cfc3bc879b00529085024fb7ef3b2987b08c
Nick Czarnecki <nick@deliverycircle.com>
2025-01-12 21:33:53 -0500

    create htmlnode class and tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from textnode import TextNode
 from textnode import TextType
+from htmlnode import HTMLNode
 
 
 def main():
     node = TextNode("Test", TextType.NORMAL, "www.google.com")
     print(node.__repr__())
+
+    node2 = HTMLNode("div", "This is a text node", None, {
+        "href": "https//www.google.com",
+        "target": "_blank",
+    })
+
+    print(node2.__repr__())
+    print(node2.props_to_html())
 
 
 main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+
+    def test_repr(self):
+        node = HTMLNode("div", "This is a test node", None, {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        })
+
+        self.assertEqual(
+            repr(node), "HTMLNode(div, This is a test node, None, {'href': 'https://www.google.com', 'target': '_blank'})")
+
+    def test_props_to_html(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node = HTMLNode("div", "This is a test node", None, props)
+        node.props_to_html()
+        self.assertEqual(node.props_to_html(),
+                         ' href="https://www.google.com" target="_blank"')
+
+    def test_to_html_raises_not_implemented(self):
+        node = HTMLNode("div", "This is a test node", None, {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        })
+        self.assertRaises(NotImplementedError, node.to_html)
+
+
+if __name__ == "__main__":
+    unittest.main()
